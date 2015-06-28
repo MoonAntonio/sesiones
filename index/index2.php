d95699a40d40c20d67056387010c53c53f5d9bda
lPinchol <pinchodev91@gmail.com>
2015-06-28 18:39:16 +0200

    Delete includes and move class.Conexion.php

---
+++ index/index2.php
@@ -0,0 +1,37 @@
+<?php
+	#Test gitHub
+	if(isset($_GET['error']))
+	{
+		if($_GET['error'] == 1)
+		{
+			echo 'ERROR: Debes llenar todos los campos';
+		}else if($_GET['error'] == 2)
+		{
+			echo 'ERROR: Los datos no son correctos';
+		}else if($_GET['error'] == 3)
+		{
+			echo 'ERROR: No intentes saltarte el inicio';
+		}else if($_GET['error'] == 4)
+		{
+			echo 'ERROR: No intentes iniciar sin iniciar sesion';
+		}
+		
+		unset($_GET['error']);
+	}
+?>
+
+<html>
+	<head>
+    	<title>Acceso de usuario</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+    	<form action="login.php" method="post">
+        	<label>Usuario: </label><input type="text" name="user" /><br />
+            <label>Contraseña: </label><input type="text" name="pass" /><br />
+            <label><input type="checkbox" name="sesion" value="1" />Deseo recordar mis datos</label><br />
+            <input type="submit" value="Clickame aqui" />
+        </form>
+    </body>
+</html>
